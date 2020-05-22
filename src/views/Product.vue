a600e7ec4d846559ed8f2902e079610b0658f242
mhafet17 <do.crazy192@gmail.com>
2020-05-22 09:35:47 +0700

    create views product

---
+++ src/views/Product.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="product">
+   <HeaderShayna/>
+   <FooterShayna/>
+</div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import FooterShayna from '@/components/FooterShayna.vue';
+
+
+export default {
+  name: "product",
+  components: {
+    HeaderShayna,
+    FooterShayna
+   
+  }
+}
+</script>
